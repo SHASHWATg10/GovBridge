9e93d8f5cecedbd315bfd3b11dd528f1843224b2
Shashwat Gupta <shashwat.gupta.sg10@gmail.com>
2026-04-16 12:13:32 +0530

    Updated directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import React, { Component } from "react";
-import Home from "./Homes";
-import About from "./About";
-import Contact from "./Contact";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import ContactUs from './pages/ContactUs';
+import EligibilityCheck from './pages/EligibilityCheck';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { AuthProvider } from './context/AuthContext';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Home />
-        <About />
-        <Contact />
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="app-layout">
+          <Navbar />
+          <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/eligibility" element={<EligibilityCheck />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-    );
-  }
+    </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
-
